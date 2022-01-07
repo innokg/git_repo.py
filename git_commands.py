2b6359a22b61983cd5201a83ddc864638b9c8412
Ulan Chokushov <ulan.chokushov@gmail.com>
2022-01-07 12:07:14 +0600

    initial commit

---
+++ git_commands.py
@@ -0,0 +1,17 @@
+
+git init - .git
+git remoted add name url - add repository to github
+git pull origin master 
+git status 
+git add name file 
+gid add . - adding all fies was changed
+git commit 
+git commit -m "comment"
+git branch 
+git branch name branch
+git checkout name branch
+git push name branch
+git push origin master 
+git reset filename 
+ 
+
